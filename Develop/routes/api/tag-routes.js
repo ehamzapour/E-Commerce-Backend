4c06aa1da706c970ca1e0c04b580cb013acf0846
Elham Hamzapour <ehamzapour@gmail.com>
2023-01-12 18:34:04 -0600

    fixed errors in tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Tag }]
+      include: [{ model: Product }]
     });
     
     res.status(200).json(tagData);
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk({
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
